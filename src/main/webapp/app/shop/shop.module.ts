9e14cb8ea8e478373f67ba4138291b7ac22cf5e1
m2gi-liju <junhao.li@etu.univ-grenoble-alpes.fr>
2022-12-08 11:34:22 +0100

    test

--- src/main/webapp/app/shop/shop.module.ts
+++ src/main/webapp/app/shop/shop.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 import { ShopRoutingModule } from './shop-routing.module';
 
@@ -56,6 +57,5 @@ import { BuyerOrderComponent } from './buyer-order/buyer-order.component';
     ButtonModule,
     DataViewModule,
   ],
-  imports: [CommonModule, ShopRoutingModule],
 })
 export class ShopModule {}
