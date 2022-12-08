32eb885b5af1693e09326b8928a3aca145fdba00
m2gi-liju <junhao.li@etu.univ-grenoble-alpes.fr>
2022-12-08 11:34:22 +0100

    test

--- src/main/webapp/app/shop/shop.module.ts
+++ src/main/webapp/app/shop/shop.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+
 import { ShopRoutingModule } from './shop-routing.module';
 
 import { AccueilComponent } from './accueil/accueil.component';
@@ -8,9 +9,10 @@ import { TopNavComponent } from './top-nav/top-nav.component';
 import { ProductCardComponent } from './product/product-card/product-card.component';
 import { ProductDetailComponent } from './product/product-detail/product-detail.component';
 import { MainBodyComponent } from './main-body/main-body.component';
+import { SharedModule } from 'app/shared/shared.module';
 
 @NgModule({
   declarations: [AccueilComponent, TopNavComponent, ProductCardComponent, ProductDetailComponent, MainBodyComponent],
-  imports: [CommonModule, ShopRoutingModule, RouterModule],
+  imports: [CommonModule, SharedModule, ShopRoutingModule, RouterModule],
 })
 export class ShopModule {}
