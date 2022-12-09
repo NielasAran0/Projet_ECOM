c21e79edd038d8ee0bdd63912e7433bfcd4221d7
NielasAran0 <morand.lucas@yahoo.fr>
2022-12-09 14:11:49 +0100

    changed import links

--- src/main/webapp/app/shop/shop-routing.module.ts
+++ src/main/webapp/app/shop/shop-routing.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SalesPostRoutingResolveService } from 'app/entities/sales-post/route/sales-post-routing-resolve.service';
-import { PaymentEchecComponent } from 'app/payment-echec/payment-echec.component';
-import { PaymentSuccesComponent } from 'app/payment-succes/payment-succes.component';
+import { PaymentEchecComponent } from './payment-echec/payment-echec.component';
+import { PaymentSuccesComponent } from './payment-succes/payment-succes.component';
 import { PaymentComponent } from 'app/shop/payment/payment.component';
-import { PersonalInfoComponent } from 'app/personal-info/personal-info.component';
+import { PersonalInfoComponent } from './personal-info/personal-info.component';
 import { AccueilComponent } from './accueil/accueil.component';
 import { MainBodyComponent } from './main-body/main-body.component';
 import { ProductDetailComponent } from './product/product-detail/product-detail.component';
