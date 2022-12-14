f49cd1c9ea7187e20e0aac53405e1d8cc71b5063
NielasAran0 <morand.lucas@yahoo.fr>
2022-12-14 23:33:37 +0100

    ajout info personelles dans db

--- src/main/webapp/app/shop/payment/payment.component.ts
+++ src/main/webapp/app/shop/payment/payment.component.ts
@@ -1,6 +1,8 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { AppUserTransmissionService } from '../app-user-transmission.service';
 
 @Component({
   selector: 'jhi-payment',
@@ -14,10 +16,16 @@ export class PaymentComponent {
     cvv: new FormControl('', [Validators.required, Validators.pattern('[0-9]{3}')]),
   });
 
-  constructor(private _router: Router) {}
+  constructor(private _router: Router, private http: HttpClient, private appUserTransmissionService: AppUserTransmissionService) {}
   //ngOnInit(): void {}
 
   onSubmit(): void {
+    let tel = this.appUserTransmissionService.getTel();
+    let nom = this.appUserTransmissionService.getNom();
+    let prenom = this.appUserTransmissionService.getPrenom();
+    let addresse = this.appUserTransmissionService.getAddresse();
+    this.http.post('http://localhost:8080/api/app-user-info', [tel, addresse]).subscribe(response => {});
+    this.http.put('http://localhost:8080/api/payment', []);
     this._router.navigate([this._router.url + '/succes']);
   }
 }
