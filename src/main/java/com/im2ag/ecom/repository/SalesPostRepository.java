386e30939d78ae6425b34e7e4c895a1bbd685f93
m2gi-liju <junhao.li@etu.univ-grenoble-alpes.fr>
2022-12-15 18:30:18 +0100

    add missing imports

--- src/main/java/com/im2ag/ecom/repository/SalesPostRepository.java
+++ src/main/java/com/im2ag/ecom/repository/SalesPostRepository.java
@@ -1,6 +1,8 @@
 package com.im2ag.ecom.repository;
 
 import com.im2ag.ecom.domain.SalesPost;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.*;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
